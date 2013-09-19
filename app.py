fdc95858ecf4e5a953abeb2d3a26a49899aae007
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-09-19 20:16:59 +0200

    404s are evil
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -15,6 +15,14 @@ def index():
         ('we', 'need', 'even', 'more', '!'),
     )
     return render_template('index.html', data=data)
+
+@app.route('/add/', methods=['GET', 'POST'])
+def edit():
+    if request.method == 'POST':
+        # Temporary.
+        return render_template('add.html')
+    else:
+        return render_template('add.html')
 
 @app.route('/edit/')
 def edit():
