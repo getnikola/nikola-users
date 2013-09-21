182894a8af9b37ba812aaa2a3a3a2b5d7923b5c8
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-09-21 12:51:36 +0200

    debug
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- app.py
+++ app.py
@@ -111,4 +111,4 @@ def admin_panel():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=bool(os.environ["DEBUG"]))
