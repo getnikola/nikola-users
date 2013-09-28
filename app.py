188a91bd6d29ff5d5fbacde549b590c893d6ed45
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-09-28 17:12:51 +0200

    have your array, mandrill
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- app.py
+++ app.py
@@ -309,7 +309,7 @@ Some Sites Using Nikola at {ssun}
 
     message = {'text': msg, 'from_email': 'users@getnikola.com', 'from_name':
                'Some Sites Using Nikola ({0})'.format(page.author), 'to':
-               'kwpolska@gmail.com', 'auto_html': True}
+               [{'email': 'kwpolska@gmail.com', 'name': 'Nikola'}], 'auto_html': True}
     sendtime = datetime.datetime.utcnow().isoformat()
     return MANDRILL.messages.send(message=message, async=False, ip_pool='Main Pool', send_at=sendtime)
 
