60084fc8445a918171a73d20385c8f232dba03ee
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-09-28 13:54:26 +0200

    update
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- add-language.py
+++ add-language.py
@@ -3,5 +3,11 @@
 import app
 import sys
 
+print('[{0}] {1}'.format(sys.argv[2], sys.argv[1]))
 app.db.session.add(app.Language(sys.argv[1], sys.argv[2]))
-app.db.session.commit()
+try:
+    app.db.session.commit()
+except:
+    raise
+else:
+    print('OK')
