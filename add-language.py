e65c9bebb2e41dc20b127b8756c581ed1c2e9d15
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-10-14 14:35:32 +0200

    civilize the add-language.py script
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- add-language.py
+++ add-language.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
-import app
 import sys
+
+if len(sys.argv) != 3:
+    print('USAGE: add-language.py English en')
+    exit(-1)
+
+import app
 
 print('[{0}] {1}'.format(sys.argv[2], sys.argv[1]))
 app.db.session.add(app.Language(sys.argv[1], sys.argv[2]))
@@ -11,3 +16,5 @@ except:
     raise
 else:
     print('OK')
+    print('Make sure to add the flag!')
+    print('(If you already added one, run: `heroku ps:restart`)')
