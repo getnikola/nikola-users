3bec390daf75f61f6d79a641d33ac1ec42201770
Chris Warrick <kwpolska@gmail.com>
2016-07-26 17:34:25 +0200

    Pull from github in deploy script
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 source /srv/users.getnikola.com/bin/activate
 source local-config
 ./manage.py collectstatic --noinput
+cd /home/kwpolska/git/nikola-users
+git pull origin master
 cd /srv/users.getnikola.com/appdata
 git pull origin master
 sudo systemctl restart uwsgi
