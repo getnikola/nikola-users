f3d826c0c3be9a6f84cfc963dd412e15765abb40
Chris Warrick <kwpolska@gmail.com>
2016-09-23 18:15:21 +0200

    Use a different e-mail for notifications because Zoho hates me
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- sites/views.py
+++ sites/views.py
@@ -125,7 +125,7 @@ def add(request):
                     '{0} <{1}> has requested addition of "{2}" <{3}> to the Nikola Users site.\n'
                     'Please visit the admin panel to accept or reject it: https://users.getnikola.com/admin/'.format(
                         site.author, site.email, site.title, site.url),
-                    'nikola-users@chriswarrick.com', ['users@getnikola.com'], fail_silently=False)
+                    'nikola-users@chriswarrick.com', ['nikola-users@chriswarrick.com'], fail_silently=False)
                 context['email_succeeded'] = True
             except Exception:
                 context['email_succeeded'] = False
