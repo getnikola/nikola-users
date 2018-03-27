788633f66de9009a2b4bafbcd248bd3620563e1f
Chris Warrick <kwpolska@gmail.com>
2018-03-27 18:51:11 +0200

    Order languages in admin form
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- sites/admin.py
+++ sites/admin.py
@@ -1,13 +1,22 @@
 """Admin registrations for Sites."""
 
 from django.contrib import admin
+from django.forms import ModelForm, ModelMultipleChoiceField
 from .models import Site, Language
+
+class SiteAdminForm(ModelForm):
+    languages = ModelMultipleChoiceField(queryset=Language.objects.order_by('name'))
+
+    class Meta:
+        model = Site
+        fields = '__all__'
 
 class LanguageAdmin(admin.ModelAdmin):
     list_display = ('name', 'code', 'language_code', 'country_code', 'display_country')
 
 class SiteAdmin(admin.ModelAdmin):
     list_display = ('title', 'clickable_url', 'author', 'visible', 'featured')
+    form = SiteAdminForm
 
     def make_visible(modeladmin, request, queryset):
         queryset.update(visible=True)
