cc86a342934ed4227a0dba3f02f94576efa59cfb
Chris Warrick <kwpolska@gmail.com>
2026-02-06 00:01:50 +0100

    Improve docker performance

--- docker/docker-cmd.sh
+++ docker/docker-cmd.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-/venv/bin/python manage.py collectstatic --noinput
-/venv/bin/python manage.py migrate
-exec /venv/bin/python -m gunicorn --bind 0.0.0.0:6868 nikolausers.asgi:application -k uvicorn_worker.UvicornWorker
+statefile=/tmp/migrated
+if [ ! -f "$statefile" ]; then
+    /venv/bin/python manage.py collectstatic --noinput
+    /venv/bin/python manage.py migrate --noinput
+    touch "$statefile"
+fi
+exec /venv/bin/python -m gunicorn --bind 0.0.0.0:6868 nikolausers.asgi:application -k uvicorn_worker.UvicornWorker -w 3
